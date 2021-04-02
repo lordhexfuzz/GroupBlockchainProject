d314968e694ae72384f1591c5fbbc2cf0469b6a7
Lynn Varga <lynnkvarga@gmail.com>
2021-04-02 15:47:56 -0400

    22:53

--- src/app/components/block-view/block-view.component.ts
+++ src/app/components/block-view/block-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-block-view',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./block-view.component.css']
 })
 export class BlockViewComponent implements OnInit {
+
+  @Input() public block;
 
   constructor() { }
 
