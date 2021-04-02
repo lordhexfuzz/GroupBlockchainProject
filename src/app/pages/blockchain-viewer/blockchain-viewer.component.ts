6696e06a3775106192a5589084785bf403580f9a
Lynn Varga <lynnkvarga@gmail.com>
2021-04-02 15:29:28 -0400

    Added blockchain service + page @ 14:44

---
+++ src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blockchain-viewer',
+  templateUrl: './blockchain-viewer.component.html',
+  styleUrls: ['./blockchain-viewer.component.css']
+})
+export class BlockchainViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Blockchain } from 'SaadChain/src/blockchain';
+import EC from "elliptic";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
 
-  constructor() { }
+  public blockchainInstance = new Blockchain();
+  public walletKeys = [];
+
+  constructor() {
+    this.blockchainInstance.difficulty = 1;
+    this.blockchainInstance.minePendingTransactions('my-wallet-address');
+
+    this.generateWalletKeys();
+   }
+
+   getBlocks(){
+      return this.blockchainInstance.chain;
+   }
+
+   private generateWalletKeys(){
+     const ec = new EC.ec('secp256k1');
+     const key = ec.genKeyPair();
+
+     this.walletKeys.push({
+       keyObj: key,
+       publicKey: key.getPublic('hex'),
+       privateKey: key.getPrivate('hex')
+     });
+   }
 }
